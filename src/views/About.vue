beddd52c0ae0efeb4d54eaba039ab49c9ae0c1f3
Robin Williscroft <robin.williscroft@lynkco.com>
2022-10-19 21:29:55 +0200

    Initial Commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetodo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, explicabo
+      eveniet sequi doloremque fugit cumque dolorum quibusdam vitae voluptate
+      voluptatem nesciunt corporis atque sapiente quae dicta esse, rerum
+      distinctio provident.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, explicabo
+      eveniet sequi doloremque fugit cumque dolorum quibusdam vitae voluptate
+      voluptatem nesciunt corporis atque sapiente quae dicta esse, rerum
+      distinctio provident.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, explicabo
+      eveniet sequi doloremque fugit cumque dolorum quibusdam vitae voluptate
+      voluptatem nesciunt corporis atque sapiente quae dicta esse, rerum
+      distinctio provident.
+    </p>
   </div>
 </template>
